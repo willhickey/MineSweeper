c6c0ccfa3ad2dd16118f3eefff789ff34207236c
Will Hickey <csu_hickey@yahoo.com>
2018-01-05 16:08:56 -0700

    finished initial development

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,20 +1,70 @@
 from msvcrt import getch
 from MinesweeperClasses import GameGrid
+import os
+import sys
+from ctypes import *
+STD_OUTPUT_HANDLE = -11
+class COORD(Structure):
+	pass
+ 
+COORD._fields_ = [("X", c_short), ("Y", c_short)]
+ 
+def print_at(r, c, s):
+	h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+	windll.kernel32.SetConsoleCursorPosition(h, COORD(c, r))
+	c = s.encode("windows-1252")
+	windll.kernel32.WriteConsoleA(h, c_char_p(c), len(c), None, None)
+os.system('cls')
 
-grid = GameGrid(5,5,24)
-print(grid)
-grid.layMines(1,1)
+gridHeight = 10
+gridWidth = 10
+grid = GameGrid(gridHeight,gridWidth,10)
+#print(grid)
+#grid.layMines(1,1)
 print(grid)
 
-# while True:
-	# #draw the cursor border
-	# print_at(2*cursorY, 2*cursorX, '===')
-	# print_at(2*cursorY+2, 2*cursorX, '===')
-	# print_at(2*cursorY+1, 2*cursorX, '$ $')
-	# key = getch()
-	# if key == b'Q' or key == b'q':
-		# break
-	# if key == b' ':
-		# sweep(cursorX, cursorY)
-	# if key == b'F':
-		# markMine(cursorX, cursorY)
+cursorX = 0
+cursorY = 5
+minesPositioned = 0
+while True:
+	#draw the cursor border
+	os.system('cls')
+	print(grid)
+	print_at(2*cursorY, 2*cursorX, '===')
+	print_at(2*cursorY+2, 2*cursorX, '===')
+	print_at(2*cursorY+1, 2*cursorX+2, '$')
+	print_at(2*cursorY+1, 2*cursorX, '$')
+	key = getch()
+	if key == b'Q' or key == b'q':
+		break
+	if key == b' ':
+		if minesPositioned ==0:
+			minesPositioned = 1
+			grid.layMines(cursorY,cursorX)
+			
+		detonatedMine = grid.revealSquare(cursorY, cursorX)
+		if detonatedMine == 1:
+			os.system('cls')
+			print(grid)
+			print_at(2*gridHeight+3, 1, 'Detonated a mine\n')
+			sys.exit()
+		gameWon = grid.checkGrid()
+		if gameWon == 1:
+			os.system('cls')
+			print(grid)
+			print_at(2*gridHeight+3, 1, 'You win!\n')
+			sys.exit()
+			
+	if key == b'F' or key == b'f':
+		grid.markMine(cursorY, cursorX)
+		
+	#move the cursor if an arrow key was pressed
+	if key == b'K' and cursorX > 0:
+		cursorX = cursorX - 1
+	if key == b'M' and cursorX < gridWidth-1:
+		cursorX = cursorX + 1
+	if key == b'H' and cursorY > 0:
+		cursorY = cursorY - 1
+	if key == b'P' and cursorY < gridHeight-1:
+		cursorY = cursorY + 1		
+	
--- MinesweeperClasses.py
+++ MinesweeperClasses.py
@@ -33,26 +33,52 @@ class GameGrid:
 	
 	def __str__(self):
 		stringRepresentation = ''
-		for rowIndex in range(0, self.gridHeight):
-			for colIndex in range(0, self.gridWidth):
-				stringRepresentation = stringRepresentation + self.grid[rowIndex][colIndex].__str__()
-			stringRepresentation = stringRepresentation + '\n'
+		#for rowIndex in range(0, self.gridHeight):
+		#	for colIndex in range(0, self.gridWidth):
+		#		stringRepresentation = stringRepresentation + self.grid[rowIndex][colIndex].__str__()
+		#	stringRepresentation = stringRepresentation + '\n'
+		for y in range(0,self.gridHeight):
+			stringRepresentation = stringRepresentation + '-'*self.gridWidth*2 + '-' + '\n' 
+			for x in range(0,self.gridWidth):
+				stringRepresentation = stringRepresentation + '|' + self.grid[y][x].__str__()
+			stringRepresentation = stringRepresentation + '|\n'
+		stringRepresentation = stringRepresentation + '-'*self.gridWidth*2 + '-' + '\n' 
 		return stringRepresentation
 	
-	#def revealSquare(self, clickedCol, clickedRow):
+	def revealSquare(self, clickedY, clickedX):
+		if self.grid[clickedY][clickedX].isFlagged == 1:
+			return 0
+		self.grid[clickedY][clickedX].isRevealed = 1
+		if self.grid[clickedY][clickedX].isMine == 1:
+			return 1
 		
-	#def flagSquare(self)
-	#move position
-	#reveal square
-	#populate grid
+		if self.grid[clickedY][clickedX].adjacentMineCount == 0:
+			for i in range(-1, 2):
+				for j in range(-1, 2):
+					if clickedY+i >=0 and clickedY+i < self.gridHeight and clickedX+j>=0 and clickedX+j < self.gridWidth:
+						self.grid[clickedY+i][clickedX+j].isRevealed = 1
+		return 0
+
+		
+	def markMine(self, clickedY, clickedX):
+		self.grid[clickedY][clickedX].isFlagged = 1
+	
+	def checkGrid(self):
+		for y in range(0,self.gridHeight):
+			for x in range(0,self.gridWidth):
+				if not self.grid[y][x].isRevealed and not self.grid[y][x].isMine:
+					return 0
+		return 1	#if we get here, the entire grid is either revealed or mines
 	
 class GameSquare:
 	isMine = 0
 	isFlagged = 0
-	isRevealed = 1
+	isRevealed = 0
 	adjacentMineCount = 0
 	
 	def __str__(self):
+		if self.isFlagged == 1:
+				return 'F'
 		if self.isRevealed == 1:
 			if self.isMine == 1:
 				return 'X'
